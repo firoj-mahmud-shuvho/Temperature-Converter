374a8fe163531d40b64c8a812fe816fea83adca4
Firoj Mahmud Shuvho <mahmudshuvho111@gmail.com>
2024-07-28 16:44:34 +0600

    Temaparature Converter

---
+++ bin/temperature_converter.dart
@@ -0,0 +1,5 @@
+import 'package:temperature_converter/auto_generated.dart' as temperature_converter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${temperature_converter.calculate()}!');
+}
---
+++ test/temperature_converter_test.dart
@@ -0,0 +1,8 @@
+import 'package:temperature_converter/auto_generated.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
